f040971b84ba3fdf6edcf4be6c1d3e6e7c526fbd
Aoi Emerauda <aoi@emerauda.com>
2025-06-23 17:05:22 +0900

    報酬計算テストの期待値を更新し、ブロック報酬に基づく合計金額を修正しました。これにより、テストの正確性が向上します。

--- payouts/unlocker_test.go
+++ payouts/unlocker_test.go
@@ -16,11 +16,11 @@ func TestMain(m *testing.M) {
 func TestCalculateRewards(t *testing.T) {
 	blockReward, _ := new(big.Rat).SetString("8000000000000000000")
 	shares := map[string]int64{"0x0": 1000000, "0x1": 20000, "0x2": 5000, "0x3": 10, "0x4": 1}
-	expectedRewards := map[string]int64{"0x0": 4877996431, "0x1": 97559929, "0x2": 24389982, "0x3": 48780, "0x4": 4878}
+	expectedRewards := map[string]int64{"0x0": 7804794290, "0x1": 156095886, "0x2": 39023971, "0x3": 78048, "0x4": 7805}
 	totalShares := int64(1025011)
 
 	rewards := calculateRewardsForShares(shares, totalShares, blockReward)
-	expectedTotalAmount := int64(5000000000)
+	expectedTotalAmount := int64(8000000000)
 
 	totalAmount := int64(0)
 	for login, amount := range rewards {
